b39ddcde10048b9864f54ad4340c89851e4f0ad2
Timothy Altemus <tim@tastypool.com>
2022-10-31 17:34:23 -0500

    Fill out README and thoroughly add documentation

--- apps/website/src/components/Download.tsx
+++ apps/website/src/components/Download.tsx
@@ -2,12 +2,13 @@ export type DownloadProps = {
   children: any;
 };
 
+/** Download button that opens the broswer print dialog. */
 export default ({ children }: DownloadProps) => {
   const download = () => {
     window.print();
   };
   return (
-    <button class="no-print" onClick={download}>
+    <button class="no-print" onClick={download} aria-label="download">
       {children}
     </button>
   );
